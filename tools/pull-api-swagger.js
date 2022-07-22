a1a3c2489f6ba158fe88dcac4fb551fdf3fbe221
Mario Costa <mario.junior.pro@gmail.com>
2022-07-22 17:35:06 -0300

    feat: pull swagger api file

---
+++ tools/pull-api-swagger.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const fs = require('fs');
+const axios = require('axios');
+
+const FILENAME = 'api-swagger.json';
+const OUTPUT_PATH = path.resolve(process.cwd(), './src/api');
+
+async function run() {
+  const ENDPOINT = process.env.SWAGGER_JSON_PATH || 'http://localhost:6007/sdk-json';
+  const { data } = await axios.get(ENDPOINT);
+
+  const fullPath = path.resolve(OUTPUT_PATH, FILENAME);
+  fs.writeFileSync(fullPath, JSON.stringify(data, null, 2));
+
+  console.log(`Generated ${FILENAME} to ${fullPath}`);
+}
+
+run().catch((e) => console.error(e));
